5546d5de428270d99620b651b2b2cc6a7f8f1856
Denis <ar.denis.by@gmail.com>
2026-04-07 21:49:37 +0300

    add del function to user repository

--- app/users/repository.py
+++ app/users/repository.py
@@ -1,5 +1,7 @@
 from typing import TYPE_CHECKING
 
+from app.core.validations.exceptions import ElementNotFoundError
+
 if TYPE_CHECKING:
     from app.users.domain import UserBase
 
@@ -13,8 +15,14 @@ class UserRepository:
     def add_user(self, user: 'UserBase') -> None:
         self._users[user.item_id] = user
 
-    def get_user(self, key: str) -> 'UserBase':
-        return self._users.get(key)
+    def get_user(self, user_id: str) -> 'UserBase':
+        return self._users.get(user_id)
 
     def get_all_users(self) -> list['UserBase']:
         return list(self._users.values())
+
+    def del_user(self, user_id) -> None:
+        if not user_id in self._users.keys():
+            raise ElementNotFoundError(f'user #{user_id} not found')
+
+        del self._users[user_id]
